098fcbcbbc750ebad29d724ac8a9b32455e1e70a
enkonsierto <carloslpg@hotmail.com>
2022-09-28 00:11:02 +0200

    Update blueretro_presets.js

--- blueretro_presets.js
+++ blueretro_presets.js
@@ -370,7 +370,7 @@ function onlyUnique(value, index, self) {
 }
 
 function initBlueRetroCfg() {
-    getMapList('https://api.github.com/repos/darthcloud/BlueRetroWebCfg/contents/map/')
+    getMapList('https://api.github.com/repos/enkonsierto/BlueRetroWebCfg/contents/map/')
     .then(files => {
         return fetchMap(presets, files, 0);
     })
